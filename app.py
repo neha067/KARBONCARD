cb1af5cec770df49da794b687d5eab901042e4cc
Neha <nehapriyaekka2000.com>
2024-01-23 15:59:41 +0530

    zeet

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def display():
     ds = session.get('result')
     return render_template('result.html',ds=ds)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
